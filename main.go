5a5d1253ecf9ce1614bf89c1dd68e9681ed5f8c7
Shu YAMANI <mail@shuymn.me>
2026-03-24 15:24:22 +0900

    fix: wire CLI args from os.Args

--- main.go
+++ main.go
@@ -11,10 +11,11 @@ import (
 
 func main() {
 	exitCode, err := cli.Run(context.Background(), &cli.Options{
-		Args:   nil,
-		Stdin:  nil,
-		Stdout: nil,
-		Stderr: nil,
+		Args:    os.Args[1:],
+		Stdin:   nil,
+		Stdout:  nil,
+		Stderr:  nil,
+		WorkDir: "",
 	})
 	if err != nil && !errors.Is(err, cli.ErrLintFailed) {
 		_, _ = fmt.Fprintln(os.Stderr, err)
